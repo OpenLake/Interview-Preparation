ce97850c0d2dc40304846bed8a93c7d08baff378
Gaurav-Verma07 <cosmosian07@gmail.com>
2023-01-16 22:57:36 +0530

    feat: Railway API path integrated

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-const port = process.env.PORT || 3011;
+dotenv.config({ path: './.config.env' });
+const port = process.env.PORT;
 
 const Pusher = require('pusher');
 
@@ -17,7 +18,6 @@ pusher.trigger('my-channel', 'my-event', {
   message: 'hello world',
 });
 
-dotenv.config({ path: './.config.env' });
 
 mongoose.connect(process.env.DATABASE).then(() => {
   console.log('DB connected successfully!!!');
--- frontend/src/utils/apiRequests.ts
+++ frontend/src/utils/apiRequests.ts
@@ -1,8 +1,9 @@
-const serverURL = 'http://localhost:3011';
+const serverURL = 'interview-preparation-production.up.railway.app/v1';
 
 export const queData = async (type: any) => {
   try {
-    const data = await fetch(`${serverURL}/v1/home/${type}`);
+    console.log(`${serverURL}/home/${type}`);
+    const data = await fetch(`${serverURL}/home/${type}`);
     const responseData = await data.json();
     return responseData.data;
   } catch (err) {
@@ -12,7 +13,7 @@ export const queData = async (type: any) => {
 
 export const registerUser = async (userDetails: any) => {
   try {
-    const data = await fetch(`${serverURL}/v1/users/signup`, {
+    const data = await fetch(`${serverURL}/users/signup`, {
       headers: {
         'Content-type': 'application/json',
       },
@@ -26,7 +27,7 @@ export const registerUser = async (userDetails: any) => {
 
 export const loginUser = async (userAuth: any) => {
   try {
-    const data = await fetch(`${serverURL}/v1/users/login`, {
+    const data = await fetch(`${serverURL}/users/login`, {
       headers: {
         'Content-type': 'application/json',
       },
@@ -42,7 +43,7 @@ export const loginUser = async (userAuth: any) => {
 
 export const getqestion = async (queId: any) => {
   try {
-    const response = await fetch(`${serverURL}/v1/home/que/${queId}`);
+    const response = await fetch(`${serverURL}/home/que/${queId}`);
     const responseData = await response.json();
     return responseData.data;
   } catch (err) {
@@ -52,7 +53,7 @@ export const getqestion = async (queId: any) => {
 
 export const getComments = async (queId: any) => {
   try {
-    const response = await fetch(`${serverURL}/v1/comments/${queId}`);
+    const response = await fetch(`${serverURL}/comments/${queId}`);
     const responseData = await response.json();
     return responseData.data;
   } catch (err) {
@@ -62,7 +63,7 @@ export const getComments = async (queId: any) => {
 
 export const postComment = async (commentData: any) => {
   try {
-    await fetch(`${serverURL}/v1/comments/post`, {
+    await fetch(`${serverURL}/comments/post`, {
       headers: {
         'Content-type': 'application/json',
       },
@@ -76,7 +77,7 @@ export const postComment = async (commentData: any) => {
 
 export const getUserInfo = async (userId: any) => {
   try {
-    const data = await fetch(`${serverURL}/v1/users/${userId}`);
+    const data = await fetch(`${serverURL}/users/${userId}`);
     const response = await data.json();
     console.log({ response });
     return response;
@@ -87,7 +88,7 @@ export const getUserInfo = async (userId: any) => {
 
 export const likeQuestion = async (queId: any, userId: any) => {
   try {
-    const data = await fetch(`http://localhost:3011/v1/home/que/${queId}/like`, {
+    const data = await fetch(`http://localhost:3011/home/que/${queId}/like`, {
       method: 'PUT',
       headers: {
         'Content-type': 'application/json',
@@ -103,7 +104,7 @@ export const likeQuestion = async (queId: any, userId: any) => {
 
 export const unlikeQuestion = async (queId: any, userId: any) => {
   try {
-    const data = await fetch(`http://localhost:3011/v1/home/que/${queId}/unlike`, {
+    const data = await fetch(`http://localhost:3011/home/que/${queId}/unlike`, {
       method: 'PUT',
       headers: {
         'Content-type': 'application/json',
